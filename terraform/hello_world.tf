5de6e64cb92217ab7ee951cd9fed07549cf9f0d3
Liahtaz <165938366+Liahtaz@users.noreply.github.com>
2024-08-09 14:09:59 +0300

    Does this work? ${var}

--- terraform/hello_world.tf
+++ terraform/hello_world.tf
@@ -1,5 +1,7 @@
+# Interpolation documentation from https://developer.hashicorp.com/terraform/language/expressions/strings#interpolation
+
 resource "null_resource" "default" {
   provisioner "local-exec" {
-    command = "echo var.echo_message "
+    command = "echo ${var.echo_message} "
   }
 }

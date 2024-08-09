919f9d1c51c45b2544ed1730e4925888d2e92319
Liahtaz <165938366+Liahtaz@users.noreply.github.com>
2024-08-09 18:26:11 +0300

    Update ec2.tf

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -1,4 +1,4 @@
-# Documentation https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#network-and-credit-specification-example
+# Documentation https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#spot-instance-example
 
 data "aws_ami" "this" {
   most_recent = true
@@ -19,5 +19,14 @@ resource "aws_instance" "ec2-1" {
   subnet_id     = aws_subnet.public.id
   tags = {
     Name = "ec2-1-spot"
+  }
+}
+
+resource "aws_instance" "ec2-2" {
+  ami = data.aws_ami.this.id
+  instance_type = "t4g.nano"
+  subnet_id     = aws_subnet.private.id
+  tags = {
+    Name = "ec2-2-spot"
   }
 }
